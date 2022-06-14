b20a9295f04fca775e02ac541e0f86aec81249aa
kirubasankars <kiruba.sankars@gmail.com>
2022-06-14 17:13:16 -0400

    chnages

--- tf/environments/nonprod/main.tf
+++ tf/environments/nonprod/main.tf
@@ -5,13 +5,13 @@ module "prod-us-east-1" {
   aws_region  = "us-east-1"
 
   network = {
-    "hov_security_group_id"     : "sg-00dd670623136171b",
-    "subnet_group"              : "acxio-cache-1g69297q30w77"
+    "hov_security_group_id"     : "",
+    "subnet_group"              : ""
   }
 
   project = {
-    "cluster_id"        : "acxiom-4",
-    "va_application_id" : "redis5-acxiom-accept"
+    "cluster_id"        : "",
+    "va_application_id" : ""
   }
 }
 
@@ -22,8 +22,8 @@ module "prod-us-west-2" {
   aws_region  = "us-west-2"
 
   network = {
-    "hov_security_group_id"     : "sg-00dd670623136171b",
-    "subnet_group"              : "acxio-cache-1g69297q30w77"
+    "hov_security_group_id"     : "",
+    "subnet_group"              : ""
   }
 
   project = {
